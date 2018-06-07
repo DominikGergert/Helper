c3531051dde26d3f965004a5bec5cc0968ab5064
Dominik Gergert <dominikmorlang@gmx.de>
2018-06-07 23:49:02 +0200

    Erste Methode(Soll alle Sonderzeichen eines Strings entfernen + Whitespace) Zweite Methode checkt ob es sich um eine Handy oder Telefonnummer handelt

---
+++ Helper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+
+public class Helper
+{
+    public Helper()
+    {
+
+
+
+
+
+    }
+    public string RemoveSonderzeichenAndWhitespace(string Removesonderzeichen)
+    {
+        if (Removesonderzeichen.Contains(";"))
+            Removesonderzeichen.Replace(';', ' ');
+
+        if (Removesonderzeichen.Contains("/"))
+            Removesonderzeichen.Replace('/', ' ');
+
+        if (Removesonderzeichen.Contains("("))
+            Removesonderzeichen.Replace('(', ' ');
+
+        if (Removesonderzeichen.Contains(")"))
+            Removesonderzeichen.Replace(')', ' ');
+
+        if (Removesonderzeichen.Contains("|"))
+            Removesonderzeichen.Replace('|', ' ');
+
+        if (Removesonderzeichen.Contains("{"))
+            Removesonderzeichen.Replace('{', ' ');
+
+        if (Removesonderzeichen.Contains("^"))
+            Removesonderzeichen.Replace('^', ' ');
+
+        if (Removesonderzeichen.Contains(","))
+            Removesonderzeichen.Replace(',', ' ');
+
+        if (Removesonderzeichen.Contains("="))
+            Removesonderzeichen.Replace('=', ' ');
+
+        if (Removesonderzeichen.Contains("+"))
+            Removesonderzeichen.Replace('+', ' ');
+
+        if (Removesonderzeichen.Contains("!"))
+            Removesonderzeichen.Replace('!', ' ');
+
+        if (Removesonderzeichen.Contains("§"))
+            Removesonderzeichen.Replace('§', ' ');
+
+        if (Removesonderzeichen.Contains("$"))
+            Removesonderzeichen.Replace('$', ' ');
+
+        if (Removesonderzeichen.Contains("%"))
+            Removesonderzeichen.Replace('%', ' ');
+
+        if (Removesonderzeichen.Contains(";"))
+            Removesonderzeichen.Replace(';', ' ');
+
+        if (Removesonderzeichen.Contains("]"))
+            Removesonderzeichen.Replace(']', ' ');
+
+        if (Removesonderzeichen.Contains("["))
+            Removesonderzeichen.Replace('[', ' ');
+
+        if (Removesonderzeichen.Contains(";"))
+            Removesonderzeichen.Replace(';', ' ');
+
+        if (Removesonderzeichen.Contains("?"))
+            Removesonderzeichen.Replace('?', ' ');
+
+        if (Removesonderzeichen.Contains("²"))
+            Removesonderzeichen.Replace('²', ' ');
+
+        if (Removesonderzeichen.Contains("³"))
+            Removesonderzeichen.Replace('³', ' ');
+
+        if (Removesonderzeichen.Contains("´"))
+            Removesonderzeichen.Replace('´', ' ');
+
+        if (Removesonderzeichen.Contains("#"))
+            Removesonderzeichen.Replace('#', ' ');
+
+        if (Removesonderzeichen.Contains("°"))
+            Removesonderzeichen.Replace('°', ' ');
+
+        if (Removesonderzeichen.Contains("<"))
+            Removesonderzeichen.Replace('<', ' ');
+
+        if (Removesonderzeichen.Contains(">"))
+            Removesonderzeichen.Replace('>', ' ');
+
+        if (Removesonderzeichen.Contains("@"))
+            Removesonderzeichen.Replace('@', ' ');
+
+        if (Removesonderzeichen.Contains("_"))
+            Removesonderzeichen.Replace('_', ' ');
+
+        if (Removesonderzeichen.Contains(":"))
+            Removesonderzeichen.Replace(':', ' ');
+
+        if (Removesonderzeichen.Contains("€"))
+            Removesonderzeichen.Replace('€', ' ');
+
+
+
+        Removesonderzeichen.Replace(" ", "");
+
+        return Removesonderzeichen;
+    }
+
+
+    /// <summary>
+    /// Gibt einen Bool zurück True = Telefonnnumer False = Handynummer
+    /// </summary>
+    /// <param name="pTelefonnumer"></param>
+    /// <returns></returns>
+    public bool CheckPhoneOrHandyNumber(string pTelefonnumer)
+    {
+        if (pTelefonnumer.StartsWith("01"))
+            return false;
+
+        else
+            return true;
+    }
+
+}
